65fd3111b25db706f0314e78d95284f2556ab8ac
Sammi Chong <shinyee.chong@hmcts.net>
2024-08-05 17:10:58 +0100

    Update epic field to RWA-3159

--- src/service/persistence.js
+++ src/service/persistence.js
@@ -171,7 +171,7 @@ async function createHelpRequestInJira(summary, project, user, labels) {
                 name: user // API docs say ID, but our jira version doesn't have that field yet, may need to change in future
             },
             customfield_10007: 10029, // sprint
-            customfield_10008: "RWA-695" // epic
+            customfield_10008: "RWA-3159" // epic
         }
     });
 
